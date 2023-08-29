3a908cc222c71518f70ecea6f1522d1d9261456f
seominah <seominah95@gmail.com>
2023-08-30 01:34:26 +0900

    init: tofu-project

---
+++ pages/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <NuxtLink to="/test">테스트</NuxtLink>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage',
+}
+</script>
---
+++ pages/test/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <h1>{{ data.data }}</h1>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  data() {
+    return {
+      data: ''
+    }
+  },
+  mounted() {
+    this.getTest()
+  },
+  methods: {
+    getTest() {
+      axios.get('api/tests')
+      .then((res) => {
+        this.data = res.data
+      })
+    }
+  }
+}
+</script>
