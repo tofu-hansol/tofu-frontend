b833007298f37a4ac36557ffa8c36f52e325959b
seominah <seominah95@gmail.com>
2023-10-12 01:16:45 +0900

    Fix: 동호회 페이지 수정

---
+++ components/clubShortCard.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="club-short-wrap">
+    <v-card v-for="club in clubs" :key="club"
+      class="mx-auto"
+      max-width="350"
+    >
+      <v-img
+        class="align-end text-white"
+        height="150"
+        src="https://mblogthumb-phinf.pstatic.net/MjAxODA0MTJfMjEg/MDAxNTIzNDk5NDA1MTU0.fMpVtcJCq7pSbuvFjLhK_i5SeEyJMcFdGi47YZlyBSAg.YJ-Bejc-AyyzRfCvS2Buyvc3b1g11nlgyMmLnJW5-Csg.JPEG.sports_7330/1.jpg?type=w800"
+        cover
+      >
+      </v-img>
+      <v-card-title>{{ club }}</v-card-title>
+      <v-card-subtitle class="pt-1">
+        Member 10
+      </v-card-subtitle>
+
+      <v-card-text>
+        <div>동호회 간단 설명</div>
+      </v-card-text>
+
+      <v-card-actions>
+        <v-btn elevation="0" color="#2f9283" style="color: snow; font-weight: 700; font-size: .875rem;">
+          가입하기
+        </v-btn>
+      </v-card-actions>
+    </v-card>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      clubs: [ '오르다(클라이밍 동호회)', 'Made By(베이킹 동호회)' ]
+    }
+  }
+}
+</script>
+
+<style scoped>
+.club-short-wrap {
+  display: flex;
+  margin-top: 2rem;
+}
+
+.v-card__actions {
+  padding: 0 1rem 1rem;
+}
+</style>
--- pages/club/index.vue
+++ pages/club/index.vue
@@ -22,35 +22,28 @@
       </div>
     </section>
     <section class="clubs-section">
-      <v-tabs>
-        <v-tab value="total">전체</v-tab>
-        <v-tab value="workout">운동</v-tab>
-        <v-tab value="cooking">요리</v-tab>
-      </v-tabs>
-      <div class="club-short-list">
-        <v-card>
-          <v-card-text>
-            <v-window>
-              <v-window-item value="total">
-                전체 클럽 리스트 
-              </v-window-item>
-              <v-window-item value="workout">
-                운동 클럽 리스트
-              </v-window-item>
-              <v-window-item value="cooking">
-                요리 클럽 리스트
-              </v-window-item>
-            </v-window>
-            <p>content</p>
-          </v-card-text>
-        </v-card>
-      </div>
+        <v-tabs v-model="tab" color="#2f9283">
+          <v-tab :value="all">전체</v-tab>
+          <v-tab :value="workout">운동</v-tab>
+          <v-tab :value="cooking">요리</v-tab>
+        </v-tabs>
+        <div>
+          <v-window v-model="tab">
+            <v-window-item :value="all">
+              <club-short-card/>
+            </v-window-item>
+            <v-window-item :value="workout">운동</v-window-item>
+            <v-window-item :value="cooking">요리</v-window-item>
+          </v-window>
+        </div>
     </section>
   </div>
 </template>
 
 <script>
+import clubShortCard from '../../components/clubShortCard.vue'
 export default {
+  components: { clubShortCard },
   data() {
     return {
       tab: null, 
