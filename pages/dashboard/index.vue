bb6d58e58ebb11c1c0cc4fd99ea2c395b7d31cf6
seominah <seominah95@gmail.com>
2023-11-03 00:39:03 +0900

    Fix: 대시보드 수정

--- pages/dashboard/index.vue
+++ pages/dashboard/index.vue
@@ -1,6 +1,20 @@
 <template>
   <v-container>
     <v-row class="pa-3">
+      <v-card width="100%" height="700px">
+        <v-card-title>동호회별 모임장소 분포도</v-card-title>
+        <v-card-text class="cart_3 pa-0">
+          <iframe
+            class="pa-0"
+            src="http://tofu-hansol.site:9080/map"
+            width="100%"
+            height="100%"
+            frameborder="0"
+          ></iframe>
+        </v-card-text>
+      </v-card>
+    </v-row>
+    <v-row class="pa-3">
       <v-card width="100%" height="500px">
         <v-card-title>동호회별 참석 부서 요약</v-card-title>
         <v-card-text class="cart_1 pa-0">
@@ -20,21 +34,7 @@
         <v-card-text class="cart_2 pa-0">
           <iframe
             class="pa-0"
-            src="http://tofu-hansol.site:9080/member"
-            width="100%"
-            height="100%"
-            frameborder="0"
-          ></iframe>
-        </v-card-text>
-      </v-card>
-    </v-row>
-    <v-row class="pa-3">
-      <v-card width="100%" height="700px">
-        <v-card-title>동호회별 모임장소 분포도</v-card-title>
-        <v-card-text class="cart_3 pa-0">
-          <iframe
-            class="pa-0"
-            src="http://tofu-hansol.site:9080/map"
+            src="http://tofu-hansol.site:9080/position"
             width="100%"
             height="100%"
             frameborder="0"
@@ -57,7 +57,7 @@
 }
 
 .cart_3 {
-  height: 600px;
+  height: 1000px;
   transform: scale(0.9);
 }
 </style>
